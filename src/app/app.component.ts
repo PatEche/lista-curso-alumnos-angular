6c16c23706f05ce0fd482f1c0b4d2d261dde48ad
PatEche <echeverriaple@gmail.com>
2025-05-16 18:09:12 -0300

    Elimine un import

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
 import { TablaCursosComponent } from "./components/tabla-cursos/tabla-cursos.component";
 import { TablaAlumnosComponent } from "./components/tabla-alumnos/tabla-alumnos.component"
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, TablaCursosComponent, TablaAlumnosComponent],
+  imports: [CommonModule,TablaCursosComponent, TablaAlumnosComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
